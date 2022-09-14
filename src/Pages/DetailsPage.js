305ad55a8ae49679697a20254a87befe3d7d7c35
setykarimi <setykarimi80@gmail.com>
2022-09-14 22:59:21 +0430

    add new content

--- src/Pages/DetailsPage.js
+++ src/Pages/DetailsPage.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { VscLoading } from 'react-icons/vsc'
+import NewsDetails from "../Components/News/NewsDetails";
+
 const DetailsPage = () => {
-    return ( <div>DetailsPage</div> );
+    const [news, setNews] = useState(null)
+    const { id } = useParams();
+    useEffect(() => {
+        axios.get(`http://localhost:3001/articles/${id}`)
+            .then((res) => {
+                setNews(res.data)
+            })
+            .catch((err) => console.log(err))
+    }, [news])
+
+    const renderNews = () => {
+
+        let resultNews = <div className='loading-container'><VscLoading /> <p>Loading News ...</p></div>
+        if (news) {
+            {
+                resultNews = <NewsDetails {...news}/>
+            }
+        }
+
+        return resultNews
+
+    }
+
+    return (
+    <>
+        {renderNews()}
+    </>);
 }
- 
+
 export default DetailsPage;
