82312d0abef81ed6f3f5d4a3e2cd685be2cb894b
setykarimi <setykarimi80@gmail.com>
2022-09-15 20:32:56 +0430

    add db and app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Layout from './Components/Layout/Layout';
+import NewsAdd from './Components/News/Add/NewsAdd';
 import DetailsPage from './Pages/DetailsPage';
 import HomePage from './Pages/HomePage';
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element=<HomePage /> />
           <Route path='/details/:id' element=<DetailsPage /> />
+          <Route path='/news/add' element=<NewsAdd /> />
         </Routes>
       </Layout>
     </div>
