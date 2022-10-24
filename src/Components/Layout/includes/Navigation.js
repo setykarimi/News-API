9188b3bf2067412f9e669a2f603385c8624b328d
setykarimi <setykarimi80@gmail.com>
2022-10-24 13:00:21 +0330

    edit navList items and login button

--- src/Components/Layout/includes/Navigation.js
+++ src/Components/Layout/includes/Navigation.js
@@ -1,44 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './nav.scss';
 import { BsSearch } from 'react-icons/bs';
 import { HiMenuAlt3 } from 'react-icons/hi'
 
 const Navigation = () => {
     return (
-        <nav className="custom-nav">
-            <div className="custom-nav__logo">
-                <span className="logo">N</span>
-                <span className="logo-dec">TodayNews</span>
-            </div>
+        <div className="nav-container">
+            <nav className="custom-nav">
 
-            <div className="custom-nav__items">
-                <ul>
+
+                <div className="custom-nav__logo">
+                    <span className="logo">N</span>
+                    <span className="logo-dec">TodayNews</span>
+                </div>
+
+                <div className="custom-nav__items">
+                    <ul>
+                        <li>
+                            <NavLink to='/' className={(navData) => navData.isActive ? "active-link" : ""} >News</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to='/about' className={navData => navData.isActive ? "active-link" : ""}>About Us</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to='/contact' className={navData => navData.isActive ? "active-link" : ""}>Contact Us</NavLink>
+                        </li>
+
+
+
+                    </ul>
+                </div>
+
+                <div className="custom-nav__detail">
+                    <div className="custom-nav__search-bar">
+                        <BsSearch className="icon" />
+                        <input placeholder="Search ..." />
+                    </div>
                     <li>
-                        <Link to='/'>News</Link>
+                        <NavLink to='/login' className={navData => navData.isActive ? "active-link" : ""}>Login</NavLink>
                     </li>
+                </div>
 
-                    <li>
-                        <Link to='/'>Health</Link>
-                    </li>
+                <button className="toggle-menu">
+                    <HiMenuAlt3 />
+                </button>
 
-                    <li>
-                        <Link to='/'>Polotics</Link>
-                    </li>
-
-                    <li>
-                        <Link to='/'>Entertainment</Link>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="custom-nav__search-bar">
-                <BsSearch className="icon" />
-                <input placeholder="Search ..." />
-            </div>
-            <button className="toggle-menu">
-            <HiMenuAlt3 />
-            </button>
-        </nav>
+            </nav>
+        </div>
     );
 }
 
