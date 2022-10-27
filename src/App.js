5ba22ecabc6de8c54e9791799c21b36094cb5244
setykarimi <setykarimi80@gmail.com>
2022-10-27 14:46:32 +0330

    Add login route

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Layout from './Components/Layout/Layout';
 import NewsAdd from './Components/News/Add/NewsAdd';
 import DetailsPage from './Pages/DetailsPage';
 import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path='/' element=<HomePage /> />
-          <Route path='/details/:id' element=<DetailsPage /> />
-          <Route path='/news/add' element=<NewsAdd /> />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/details/:id' element={<DetailsPage />} />
+          <Route path='/news/add' element={<NewsAdd />} />
+          <Route path='/login' element={<LoginPage />} />
         </Routes>
       </Layout>
     </div>
