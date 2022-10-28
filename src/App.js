bec830298ae1eb4c7f2f936d5f9faffe876bf462
setykarimi <setykarimi80@gmail.com>
2022-10-28 15:04:53 +0330

    add AuthProvider for routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import NewsAdd from './Components/News/Add/NewsAdd';
 import DetailsPage from './Pages/DetailsPage';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
+import Authprovider from './Provider/AuthProvider';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/details/:id' element={<DetailsPage />} />
-          <Route path='/news/add' element={<NewsAdd />} />
-          <Route path='/login' element={<LoginPage />} />
+          <Authprovider>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/details/:id' element={<DetailsPage />} />
+            <Route path='/news/add' element={<NewsAdd />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Authprovider>
         </Routes>
       </Layout>
     </div>
