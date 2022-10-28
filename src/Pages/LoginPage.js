63df12a5a8f3bb916bbb19bb4a3969a941a84a9f
setykarimi <setykarimi80@gmail.com>
2022-10-28 15:49:44 +0330

    add validationa and submit function

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,35 +1,53 @@
-import { useFormik, validateYupSchema } from "formik";
+import { useFormik } from "formik";
 import * as Yup from 'yup';
+import { useAuthActions } from "../Provider/AuthProvider";
 const LoginPage = () => {
-
+    const setAuth = useAuthActions();
     const initialValues = {
         name: '',
         password: ''
     }
 
-    const validateYupSchema = Yup.object({
+    const validationSchema = Yup.object({
         name: Yup.string().required('Name is required'),
-        password: Yup.string().required('Password is required'),
-    }) 
+        password: Yup.string().required('Password is required')
+    })
 
-    const formik = useFormik(() => {
+    const onSubmit = (values) => {
+        setAuth(values)
+    }
+
+    const formik = useFormik({
         initialValues,
-        validateYupSchema
+        onSubmit,
+        validationSchema,
+        validateOnMount: true
     })
     return (
         <>
             <h1>Login</h1>
-            <form>
-
+            <form onSubmit={formik.handleSubmit}>
                 <div>
                     <label>Name:</label>
-                    <input type="text" />
+                    <input
+                        type="text"
+                        name="name"
+                        onChange={formik.handleChange}
+                        values={formik.values.name}
+                        onBlur={formik.handleBlur}
+                    />
                     {formik.errors.name && formik.touched.name && <div className="error">{formik.errors.name}</div>}
                 </div>
 
                 <div>
                     <label>Password:</label>
-                    <input type="password" />
+                    <input
+                     type="password"
+                     name="password"
+                     onChange={formik.handleChange}
+                     values={formik.values.password}
+                     onBlur={formik.handleBlur} />
+                     {formik.errors.password && formik.touched.password && <div className="error">{formik.errors.password}</div>}
                 </div>
 
 
