d4d880ae033301dfb662bfeb00f760ce672d6826
setykarimi <setykarimi80@gmail.com>
2022-10-30 22:21:14 +0330

    add new classes

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,15 +1,15 @@
+import './input.scss'
 const Input = ({formik, label, type = "text", name}) => {
     return (
         <div className="form-group">
             <label htmlFor={label}>{label}:</label>
-            <div>
                 <input
                     type={type}
                     name={name}
                     {...formik.getFieldProps({name})}
                 />
                 {formik.errors[name] && formik.touched[name] && <div className="error">{formik.errors[name]}</div>}
-            </div>
+         
         </div>
     );
 }
