a9291eec8962ba8e1f98af5c467e7744dd15896e
setykarimi <setykarimi80@gmail.com>
2022-11-02 15:00:26 +0330

    add function and open menu class toggling menu in responsive view

--- src/Components/Layout/includes/Navigation.js
+++ src/Components/Layout/includes/Navigation.js
@@ -3,50 +3,54 @@ import './nav.scss';
 import { BsSearch } from 'react-icons/bs';
 import { HiMenuAlt3 } from 'react-icons/hi'
 import { useAuth } from "../../../Provider/AuthProvider";
+import { useState } from "react";
 
 const Navigation = () => {
     const auth = useAuth();
+    const [isShow, setIsShow] = useState("")
     return (
         <div className="nav-container">
-            <nav className="custom-nav">
+            <nav className={`custom-nav ${isShow === true ? "open-menu" : ""}`}>
                 <div className="custom-nav__logo">
                     <span className="logo">N</span>
                     <span className="logo-dec">TodayNews</span>
                 </div>
 
-                <div className="custom-nav__items">
-                    <ul>
-                        <li>
-                            <NavLink to='/' className={(navData) => navData.isActive ? "active-link" : ""} >News</NavLink>
-                        </li>
+                <div className="custom-nav__menu">
+                    <div className="custom-nav__items">
+                        <ul>
+                            <li>
+                                <NavLink to='/' className={(navData) => navData.isActive ? "active-link" : ""} >News</NavLink>
+                            </li>
 
-                        <li>
-                            <NavLink to='/about' className={navData => navData.isActive ? "active-link" : ""}>About Us</NavLink>
-                        </li>
+                            <li>
+                                <NavLink to='/about' className={navData => navData.isActive ? "active-link" : ""}>About Us</NavLink>
+                            </li>
 
-                        <li>
-                            <NavLink to='/contact' className={navData => navData.isActive ? "active-link" : ""}>Contact Us</NavLink>
-                        </li>
+                            <li>
+                                <NavLink to='/contact' className={navData => navData.isActive ? "active-link" : ""}>Contact Us</NavLink>
+                            </li>
 
-                        {auth && <li><NavLink to='/news/add' className={navData => navData.isActive ? "active-link" : ""}>Add News</NavLink></li>}
+                            {auth && <li><NavLink to='/news/add' className={navData => navData.isActive ? "active-link" : ""}>Add News</NavLink></li>}
 
 
 
-                    </ul>
-                </div>
-
-                <div className="custom-nav__detail">
-                    <div className="custom-nav__search-bar">
-                        <BsSearch className="icon" />
-                        <input placeholder="Search ..." />
+                        </ul>
                     </div>
-                    <li>
-                    
-                        <NavLink to={auth? "/profile" : "/login"} className={navData => navData.isActive ? "active-link" : ""}>{auth? "Profile" : "Login"}</NavLink>
-                    </li>
+
+                    <div className="custom-nav__detail">
+                        <div className="custom-nav__search-bar">
+                            <BsSearch className="icon" />
+                            <input placeholder="Search ..." />
+                        </div>
+                        <li>
+
+                            <NavLink to={auth ? "/profile" : "/login"} className={navData => navData.isActive ? "active-link" : ""}>{auth ? "Profile" : "Login"}</NavLink>
+                        </li>
+                    </div>
                 </div>
 
-                <button className="toggle-menu">
+                <button className="toggle-menu" onClick={() => setIsShow(prevState => !prevState)}>
                     <HiMenuAlt3 />
                 </button>
 
