27d0c10f51baf7e5cb224bc076e9efdaf499739a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-25 12:01:31 +0330

    Edit importing NewsDetails direction

--- src/Pages/DetailsPage.js
+++ src/Pages/DetailsPage.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { VscLoading } from 'react-icons/vsc'
-import NewsDetails from "../Components/News/Details/NewsDetails";
+import NewsDetails from "@/components/News/Details/NewsDetails";
 
 const DetailsPage = () => {
     const [news, setNews] = useState(null)
